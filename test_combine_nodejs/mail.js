086ffc494beb376ebe486ea768d445dce0a630c4
Chu Chien-Hua <49935340+chuchienhua@users.noreply.github.com>
2023-12-19 16:07:25 +0800

    Add files via upload

---
+++ test_combine_nodejs/mail.js
@@ -0,0 +1,50 @@
+const moment = require('moment');
+const fs = require('fs');
+const FormData = require('form-data');
+const axios = require('axios');
+const XLSL = require('xlsx-js-style');
+
+
+const getNowDatetimeString = () => {
+    return moment().format('YYYY-MM-DD HH:mm:ss');
+}
+
+const Send_Mail_URL = "http://192.168.8.104:3501/sendmail";
+
+const sendMail = (to, subject, html, cc, bcc, attachment = [], from = "AGV_Problem_Info <AGV_IOT@ccpgp.com>") => {
+    let axiosConfig = { proxy: false };
+    const formData = new FormData();
+    formData.append('to', to);
+    formData.append('subject', subject);
+    formData.append('html', html);
+    formData.append('cc', cc || "");
+    formData.append('bcc', bcc || "");
+    formData.append('from', from);
+
+    if (attachment.length) {
+        axiosConfig["Content-Type"] = "multipart/form-data";
+        attachment.forEach((attachment) => {
+            formData.append('attachment[]', fs.createReadStream(attachment));
+        });
+    } else {
+        axiosConfig["Content-Type"] = "application/x-www-form-urlencoded";
+    }
+    return axios.post(Send_Mail_URL, formData, axiosConfig);
+}
+
+const AGV_Stop_problem = async (user, carid) => {
+    const mailuser = user;
+    const mailcarid = carid;
+    const to = "朱建樺 <chienhua_chu@ccpgp.com>"
+    const subject = "AGV Stop Problem";
+    const html = `        
+        <h2>AGV連線異常問題</h2>
+        <p>使用者<code>${mailuser} </code>於AGV<code>${mailcarid}</code>派定任務實因連線中斷
+            ,車子已停止運行,請盡速檢查連線</p>
+    `;
+    const cc = "";
+    const bcc = "";
+    return sendMail(to, subject, html, cc, bcc);
+}
+
+module.exports = { AGV_Stop_problem };
