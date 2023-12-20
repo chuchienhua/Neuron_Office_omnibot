d2407c7e7e462fa3552b22dd8caa4492d152162d
Chu Chien-Hua <49935340+chuchienhua@users.noreply.github.com>
2023-12-20 17:25:40 +0800

    Delete Web_backend directory

--- Web_backend/routes/Oracle_Api.js
+++
@@ -1,9 +0,0 @@
-import axios from "axios";
-import * as oracledb from "../models/oracle/oraclecon.js";
-import express from "express";
-import { getUserInformation } from "../gettokenuser.js";
-
-const oracleRouter = express.Router();
-
-
-export default oracleRouter;
